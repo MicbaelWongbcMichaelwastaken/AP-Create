51b18a0b8f74f897233c0a6791c1b5622121f5a8
MicbaelWongbcMichaelwastaken <h.michaelwong@gmail.com>
2018-03-26 09:17:04 -0400

    Add files via upload

---
+++ ap_game.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+#screen = pygame.display.set_mode((800, 800))
+player_sprite = pygame.image.load('shipsprite.png')
+done = False
+
+class Background:
+    screen = pygame.image.load('spacebackground.png').convert()
+
+class Player:
+    screen.blit((player_sprite),(20,20))
+
+
+
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+
+        pygame.display.flip()
+
+

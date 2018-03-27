2d69d4d54db6d77ce83b8b936ef278c1c30eefd0
MicbaelWongbcMichaelwastaken <h.michaelwong@gmail.com>
2018-03-27 08:39:16 -0400

    Add files via upload

--- ap_game.py
+++ ap_game.py
@@ -4,13 +4,16 @@ from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 player_sprite = pygame.image.load('shipsprite.png')
+clock=pygame.time.Clock()
 done = False
 
 #class Background:
     #screen = pygame.image.load('spacebackground.png').convert()
 
 class Player:
-    screen.blit((player_sprite),(20,20))
+    screen.blit((player_sprite),(400,600))
+    def moveRight(self, pixels):
+        self.rect.x += pixels
 
 Player = pygame.sprite.Group()
 
@@ -19,6 +22,15 @@ while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done = True
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            Player.moveLeft(5)
+        if keys[pygame.K_RIGHT]:
+            Player.moveRight(5)
+
+
+        clock.tick(60)
 
         pygame.display.update()
 

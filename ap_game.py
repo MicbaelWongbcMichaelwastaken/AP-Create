f33c6733ff467abfcec5deaeb8f52f03779e622b
MicbaelWongbcMichaelwastaken <h.michaelwong@gmail.com>
2018-03-29 10:36:46 -0400

    Add files via upload

--- ap_game.py
+++ ap_game.py
@@ -3,19 +3,39 @@ from pygame.locals import *
 
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
-player_sprite = pygame.image.load('shipsprite.png')
 clock=pygame.time.Clock()
 done = False
+
 
 #class Background:
     #screen = pygame.image.load('spacebackground.png').convert()
 
-class Player:
-    screen.blit((player_sprite),(400,600))
-    def moveRight(self, pixels):
-        self.rect.x += pixels
+class Player():
+    def __init__(self):
+        self.image = pygame.image.load('shipsprite.png')
+        self.x = 400
+        self.y = 600
+    def player_keys(self):
+        key = pygame.key.get_pressed()
+        dist = 5
+        if key[pygame.K_RIGHT]:  # right key
+            self.x += dist  # move right
+        elif key[pygame.K_LEFT]:  # left key
+            self.x -= dist  # move left
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
 
-Player = pygame.sprite.Group()
+class Lasers():
+    def __init__(self):
+        self.image = pygame.image.load('beam.png')
+        self.x = Player()
+        sef.y = Player()
+
+
+
+
+
+Player = Player()
 
 
 while not done:
@@ -23,13 +43,9 @@ while not done:
                 if event.type == pygame.QUIT:
                         done = True
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            Player.moveLeft(5)
-        if keys[pygame.K_RIGHT]:
-            Player.moveRight(5)
-
-
+        screen.fill((0, 0, 0))
+        Player.draw(screen)
+        Player.player_keys()
         clock.tick(60)
 
         pygame.display.update()

ee7fc20fde56563ecbdd63478e103fc6edaf1935
MicbaelWongbcMichaelwastaken <h.michaelwong@gmail.com>
2018-04-09 09:34:32 -0400

    Add files via upload

--- ap_game.py
+++ ap_game.py
@@ -10,7 +10,7 @@ done = False
 #class Background:
     #screen = pygame.image.load('spacebackground.png').convert()
 
-class Player():
+class Player(pygame.sprite.Sprite):
     def __init__(self):
         self.image = pygame.image.load('shipsprite.png')
         self.x = 400
@@ -24,14 +24,25 @@ class Player():
             self.x -= dist  # move left
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
+    def fire(self):
+        laser = Lasers(self.rect.centerx, self.rect.top)
+        allsprites.add(laser)
+        lasers.add(laser)
 
-class Lasers():
-    def __init__(self):
-        self.image = pygame.image.load('beam.png')
-        self.x = Player()
-        sef.y = Player()
+class Lasers(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10,20))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
 
-
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
 
 
 
@@ -42,6 +53,9 @@ while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done = True
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        player.fire()
 
         screen.fill((0, 0, 0))
         Player.draw(screen)
